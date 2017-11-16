225a1427b13c9dddd7077ab73519475b9bdf6a2c
weqopy <weqopy@gmail.com>
2017-11-16 19:20:38 +0800

    Django QuerySet

--- mysite/learn/models.py
+++ mysite/learn/models.py
@@ -1,3 +1,7 @@
+# 每次创建模块类之后，需要在'python manage.py shell' 中
+# 运行'python manage.py makemigrations'及'python manage.py migrate'
+# 以更新数据库
+
 from django.db import models
 import ast
 
@@ -39,3 +43,34 @@ class ListField(models.TextField):
 
 class Article(models.Model):
     labels = ListField()
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    n_comments = models.IntegerField()
+    n_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return self.headline
